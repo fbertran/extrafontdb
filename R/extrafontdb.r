7b2405c9b2b3e09d639dd740594dc46c106e70f4
Frederic Bertrand <frederic.bertrand@lecnam.net>
2025-09-25 12:15:20 +0200

    Roxygen fixes to R file

--- R/extrafontdb.r
+++ R/extrafontdb.r
@@ -1,12 +1,3 @@
-#' extrafontdb package
-#'
-#' Ths package is meant to be used with the extrafont package. The
-#' extrafont package contains the code to install and use fonts, while this
-#' package contains the directory structure to hold the font database.
-#' See the \url{https://github.com/wch/extrafontdb} and
-#' \url{https://github.com/wch/extrafont} for more information.
-#'
-#' @name extrafontdb
-#' @docType package
+#' @keywords internal
 #' @aliases extrafontdb package-extrafontdb
-NULL
+"_PACKAGE"
